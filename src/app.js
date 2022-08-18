c68fcc446939446681c53a888e60b524309777aa
YFr93562 <francois.mackowski@gmail.com>
2022-08-18 12:10:11 +0200

    exercice 5

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import exercice1 from './exercice1/questions';
 // import exercice2 from './exercice2/questions'; // décommenter pour activer l'exercice
 // import exercice3 from './exercice3/questions';  // décommenter pour activer l'exercice
-// import exercice4 from './exercice4/questions';
+// import exercice4 from './exercice4/questions'; // décommenter pour activer l'exercice
+// import exercice5 from './exercice5/questions'; // décommenter pour activer l'exercice
---
+++ src/exercice5/questions/question1.js
@@ -0,0 +1,21 @@
+import data from '../data/articles';
+
+console.log('exercice 4 -quest 1 data:', data);
+
+/**
+ * Ci-dessus, un extrait des données d'un exercice meilleur dev de France
+ */
+
+/**
+ * Data contient une liste de string
+ * Cette liste de string est toujours composé d'un chiffre et de lettres qui sont séparés par un espace
+ * Le chiffre représente le nombre d'article et les lettres représentent le nom de l'article en question
+ */
+
+/**
+ * Parmi tout les articles en stock. Quel est l'article qu'il y a le plus en terme de quantité?
+ */
+
+/**
+ * Insère ton code en dessous
+ */
